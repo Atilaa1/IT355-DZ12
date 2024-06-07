d19b183ecc63c760810a63806bf4d3cd95df2923
Atilaa1 <atila.kujundzic.5000@metropolitan.ac.rs>
2024-06-08 01:17:14 +0200

    Initial commit

---
+++ src/main/java/org/example/it355dz12/It355Dz12Application.java
@@ -0,0 +1,13 @@
+package org.example.it355dz12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class It355Dz12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(It355Dz12Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/it355dz12/It355Dz12ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.it355dz12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class It355Dz12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
